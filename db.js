8bfd532e2bfd7cf534ed06a293340db9af690821
karj-sys <randeljohngonzales6@gmail.com>
2026-02-28 08:03:52 -0500

    db.js fix

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root', 
-  password: process.env.DB_PASSWORD || '', // Check if your MySQL has a password
-  database: process.env.DB_NAME || 'mental_health_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD,  
+  database: process.env.DB_NAME ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
